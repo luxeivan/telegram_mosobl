152b9e15320138c2f4b03d6145b993dd7761d46d
luxeivan <luxeivan@gmail.com>
2023-04-03 14:47:14 +0300

    commit1447

--- app.js
+++ app.js
@@ -2,24 +2,87 @@ const { Telegraf, session, Scenes: { WizardScene, Stage }, Markup } = require('t
 const { message } = require('telegraf/filters');
 require('dotenv').config()
 const axios = require('axios')
+function auth() {
+    return new Promise(
+        func1 => {
+            axios.post(process.env.URL + '/auth/local', {
+                identifier: process.env.LOGIN,
+                password: process.env.PASS
+            })
+                .then(res => {
+                    func1(res.data.jwt)
+                })
+                .catch(err => {
+                    console.log(err)
+                    func1(null)
+                })
+        })
+}
 
 // const exit_keyboard = Markup.keyboard(['exit']).oneTime()
 // const remove_keyboard = Markup.removeKeyboard()
 
 const nameHandler = Telegraf.on('text', async ctx => {
     ctx.scene.state.city = ctx.message.text
-    await ctx.reply('Напишите свою улицу')
+    await ctx.reply('Напишите свою улицу:')
     return ctx.wizard.next()
 })
 const ageHandler = Telegraf.on('text', async ctx => {
+    // console.log(ctx.message.from.id)
+    let authjwt = ''
+    await auth().then(jwt => authjwt = jwt)
+    axios.get(process.env.URL + `/telegram-subscribers?filters[userId][$eq]=${ctx.message.from.id}`, {
+        headers: {
+            Authorization: `Bearer ${authjwt}`
+        }
+    }).then(res => {
+        // console.log(ctx.message)
+        if (res.data.data.length < 1) {
+            axios.post(process.env.URL + `/telegram-subscribers`, {
+                data: {
+                    userId: '' + ctx.message.from.id,
+                    username: ctx.message.from.username,
+                    city: ctx.scene.state.city,
+                    street: ctx.message.text,
+                    firstname: ctx.message.from.first_name,
+                    lastname: ctx.message.from.last_name,
+                    activeSubscription: true
+                }
+            }, {
+                headers: {
+                    Authorization: `Bearer ${authjwt}`
+                }
+            })
+
+        } else {
+            axios.put(process.env.URL + `/telegram-subscribers/${res.data.data[0].id}`, {
+                data: {
+                    userId: '' + ctx.message.from.id,
+                    username: ctx.message.from.username,
+                    city: ctx.scene.state.city,
+                    street: ctx.message.text,
+                    firstname: ctx.message.from.first_name,
+                    lastname: ctx.message.from.last_name,
+                    activeSubscription: true
+                }
+            }, {
+                headers: {
+                    Authorization: `Bearer ${authjwt}`
+                }
+            })
+        }
+    }).catch(err => {
+        console.log(err.message)
+    })
+
     ctx.session.city = ctx.scene.state.city
     ctx.session.street = ctx.message.text
-    await ctx.reply('Вы успешно подписались на рассылку')
+    await ctx.reply('Вы успешно подписались на рассылку.')
     return ctx.scene.leave()
 })
 
 const infoScene = new WizardScene('infoScene', nameHandler, ageHandler)
-infoScene.enter(ctx => ctx.reply('Напишите свой город'))
+infoScene.enter(ctx => ctx.reply('Напишите свой город: '))
 
 const stage = new Stage([infoScene])
 stage.hears('exit', ctx => ctx.scene.leave())
@@ -41,15 +104,9 @@ bot.start((ctx) => {
             }
         })
 })
-
-// bot.command('city', (ctx) => {
-//     console.log(ctx.message.text.slice(6))
-// })
-// bot.command('add', (ctx) => {
-//     ctx.scene.enter('infoScene')
-// })
 bot.command('info', info)
 bot.command('subscribe', subscribe)
+bot.command('unsubscribe', unsubscribe)
 
 bot.action('subscribe', subscribe)
 bot.action('unsubscribe', unsubscribe)
@@ -59,15 +116,58 @@ function subscribe(ctx) {
     ctx.scene.enter('infoScene')
 }
 
-function unsubscribe(ctx) {
-    ctx.reply('Отписаны')
+async function unsubscribe(ctx) {
+    let authjwt = ''
+    await auth().then(jwt => authjwt = jwt)
+    axios.get(process.env.URL + `/telegram-subscribers?filters[userId][$eq]=${ctx.message.from.id}`, {
+        headers: {
+            Authorization: `Bearer ${authjwt}`
+        }
+    }).then(res => {
+        // console.log(ctx.message)
+        if (res.data.data.length < 1) {
+            ctx.reply('Вы не подписаны на рассылку по плановым отключениям.')
+
+        } else {
+            axios.put(process.env.URL + `/telegram-subscribers/${res.data.data[0].id}`, {
+                data: {
+                    userId: '' + ctx.message.from.id,
+                    username: ctx.message.from.username,
+                    firstname: ctx.message.from.first_name,
+                    lastname: ctx.message.from.last_name,
+                    activeSubscription: false
+                }
+            }, {
+                headers: {
+                    Authorization: `Bearer ${authjwt}`
+                }
+            })
+        }
+    }).catch(err => {
+        console.log(err.message)
+    })
+    ctx.reply('Вы успешно отписались от рассылки.')
 }
-function info(ctx) {
-    if (ctx.session.city && ctx.session.street) {
-        ctx.replyWithHTML(`Вы подписаны на рассылку по адресу:<b> ${capitalizeFirstLetter(ctx.session.city)} ул. ${capitalizeFirstLetter(ctx.session.street)}</b>`)
-    }else{
-        ctx.reply('Вы еще не подписаны на рассылку по плановым отключениям')
-    }
+async function info(ctx) {
+    // console.log(ctx.message)
+    let authjwt = ''
+    await auth().then(jwt => authjwt = jwt)
+    axios.get(process.env.URL + `/telegram-subscribers?filters[userId][$eq]=${ctx.message.from.id}`, {
+        headers: {
+            Authorization: `Bearer ${authjwt}`
+        }
+    }).then(res => {
+        console.log(res.data.data[0].attributes)
+        if (res.data.data.length < 1) {
+            ctx.reply('Вы не подписаны на рассылку по плановым отключениям.')
+        } else if (res.data.data[0] && !res.data.data[0].attributes.activeSubscription) {
+            ctx.reply('Вы не подписаны на рассылку по плановым отключениям.')
+        } else {
+            ctx.replyWithHTML(`Вы подписаны на рассылку по адресу:<b> ${capitalizeFirstLetter(res.data.data[0].attributes.city)} ул. ${capitalizeFirstLetter(res.data.data[0].attributes.street)}</b>`)
+        }
+    }).catch(err => {
+        console.log(err.message)
+    })
 }
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
